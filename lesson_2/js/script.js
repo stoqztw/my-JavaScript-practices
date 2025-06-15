a06db17e04d46b3c3383b5eabedc6cf6fea1fe56
atsq <ap3994741@gmail.com>
2025-06-15 23:59:13 +0300

    made new functions

--- lesson_2/js/script.js
+++ lesson_2/js/script.js
@@ -1,35 +1,94 @@
-'use strict'
+/* Задание на урок:
 
+1) Первую часть задания повторить по уроку
 
-if (-1) {
-    console.log('Ok!');
-} else {
-    console.log('Not ok!')
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
 }
 
-const num = 100;
+start();
 
-if (num < 49) {
-    console.log('error')
-} else if (num > 100) {
-    console.log('More')
-} else {
-    console.log('Ok!')
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < numberOfFilms; i++) {
+        if (numberOfFilms === 0) {
+            break;
+        };
+        let nameOfTheFilm = prompt('Какое название фильма?', ''),
+            rateOfTheFilm = prompt('На сколько оцените его?', '');
+
+        if (nameOfTheFilm != null && rateOfTheFilm != null && nameOfTheFilm != "" && rateOfTheFilm != "" && nameOfTheFilm.length < 50) {
+            personalMovieDB.movies[nameOfTheFilm] = rateOfTheFilm;
+        } else {
+            alert('Нельзя вводить пустую строку!');
+            i--;
+        }
+    };
 }
 
-(num === 50) ? console.log('ok') : console.log('error');
+rememberMyFilms();
 
-switch (num) {
-    case 49:
-        console.log('error');
-        break;
-    case '100':
-        console.log('error 2');
-        break;
-    case 50:
-        console.log('Ok');
-        break;
-    default:
-        console.log('Not now');
-        break;
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count >= 0 && personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    };
 }
+
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+function showMyDB(hidden) {
+    if (!hidden.privat) {
+        console.log(hidden);
+    } else {
+        console.log('Ошибка в доступе')
+    }
+}
+
+showMyDB(personalMovieDB);
+
+// console.log(personalMovieDB);
+
---
+++ lesson_2/js/second.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const num = 12.5;
+console.log(Math.round(num));
+console.log(Math.floor(num));
+
+const test = '12.2px';
+console.log(parseInt(test))
+console.log(parseFloat(test))
