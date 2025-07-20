399fdae3b19971c6e3e1ae0caa6822df15a66844
atsq <ap3994741@gmail.com>
2025-07-20 22:22:53 +0300

    made new lesson

--- lesson_2/js/second.js
+++ lesson_2/js/second.js
@@ -1,32 +1,43 @@
 'use strict';
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function () {
-        console.log('Hello');
-    }
-};
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 60000
+}
 
-const john = Object.create(soldier);
+function isBudgetEnough(data) {
+    let totalArea = 0,
+        totalVolume = 0,
+        priceOfPer = 0;
+    shoppingMallData.shops.forEach((item) => {
+        totalArea += item['width'] * item['length'];
+    });
+    totalVolume = totalArea * data.height;
+    priceOfPer = totalVolume * data.moneyPer1m3;
+    if (priceOfPer <= data.budget) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    };
+}
 
-// const john = {
-//     health: 100
-// };
-
-// john.__proto__ = soldier;
-
-Object.setPrototypeOf(john, soldier)
-
-console.log(john.armor);
-john.sayHello()
-
-let testObj = {
-    privat: true
-};
-
-testObj.privat = !testObj.privat;
-console.log(testObj.privat)
-
-let tetarr = ['a', 'b', 'c']
-tetarr.forEach((lit) => {console.log(Object.keys(lit))})
+console.log(isBudgetEnough(shoppingMallData))
