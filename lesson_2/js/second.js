7287a715e4a7ea2f28db606f58b4e4019fe9ccbb
atsq <ap3994741@gmail.com>
2025-07-21 22:18:23 +0300

    new lesson

--- lesson_2/js/second.js
+++ lesson_2/js/second.js
@@ -1,43 +1,20 @@
 'use strict';
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 60000
-}
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Somebody'];
 
-function isBudgetEnough(data) {
-    let totalArea = 0,
-        totalVolume = 0,
-        priceOfPer = 0;
-    shoppingMallData.shops.forEach((item) => {
-        totalArea += item['width'] * item['length'];
-    });
-    totalVolume = totalArea * data.height;
-    priceOfPer = totalVolume * data.moneyPer1m3;
-    if (priceOfPer <= data.budget) {
-        return 'Бюджета достаточно';
-    } else {
-        return 'Бюджета недостаточно';
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    let sortArr = [];
+    for (let i = 0; i <= 2; i++) {
+        sortArr[i] = arr.slice(0, 3);
+        arr = arr.slice(3);
     };
+    if (arr.length !== 0) {
+        sortArr[3] = `Оставшиеся студенты: ${arr.join(', ')}`
+    } else {
+        sortArr[3] = 'Оставшиеся студенты: -';
+    };
+    return sortArr;
 }
 
-console.log(isBudgetEnough(shoppingMallData))
+console.log(sortStudentsByGroups(students))
