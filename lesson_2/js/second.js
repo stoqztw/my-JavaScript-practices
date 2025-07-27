bc20ef3e7a52ee86b66da4ead8cd0d4b3398bc5f
atsq <ap3994741@gmail.com>
2025-07-27 15:55:42 +0300

    debugget

--- lesson_2/js/second.js
+++ lesson_2/js/second.js
@@ -1,20 +1,55 @@
 'use strict';
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Somebody'];
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-function sortStudentsByGroups(arr) {
-    arr.sort();
-    let sortArr = [];
-    for (let i = 0; i <= 2; i++) {
-        sortArr[i] = arr.slice(0, 3);
-        arr = arr.slice(3);
-    };
-    if (arr.length !== 0) {
-        sortArr[3] = `Оставшиеся студенты: ${arr.join(', ')}`
-    } else {
-        sortArr[3] = 'Оставшиеся студенты: -';
-    };
-    return sortArr;
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
 }
 
-console.log(sortStudentsByGroups(students))
+console.log(isOpen(restorantData['openNow']))
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+transferWaitors(restorantData);
