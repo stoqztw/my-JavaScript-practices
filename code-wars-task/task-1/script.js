9ab68a9fdaef62712ed04caf523b24bada1ac985
Artem <ap3994741@gmail.com>
2025-08-09 23:47:03 +0300

    code wars task

---
+++ code-wars-task/task-1/script.js
@@ -0,0 +1,12 @@
+function zeros(n) {
+	let count = 0;
+
+	while (n >= 5) {
+		n = Math.floor(n / 5);
+		count += n;
+	}
+
+	return count;
+}
+
+console.log(zeros(20))
---
+++ lesson_11/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = true;
+	document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('some.js');
+
+p[0].addEventListener('click', (e) => {
+	e.type
+})
