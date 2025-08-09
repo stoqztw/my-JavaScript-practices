5f91c16204fa44f3f1490cf62857a7618ac02875
Artem <ap3994741@gmail.com>
2025-08-09 18:27:05 +0300

    new lesson

---
+++ lesson_10/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+	const box = document.querySelector('.box');
+
+	box.addEventListener('touchmove', (e) => {
+		e.preventDefault();
+		console.log(e.targetTouches[0].pageX)
+	})
+})
