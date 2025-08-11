50b1d18625dbf6f996a2830bbd19119f13da7dce
Artem <ap3994741@gmail.com>
2025-08-11 23:05:48 +0300

    new lesson

--- lesson_13/script.js
+++ lesson_13/script.js
@@ -3,4 +3,24 @@
 const box = document.querySelector('.box');
 const block = document.querySelector('.block');
 
-console.log(block)
+console.log(block);
+
+// if (block) {
+// 	console.log(block.textContent);
+// }
+
+// console.log(block?.textContent);
+
+// console.log(1 + 2);
+
+const userData = {
+	name: 'Ivan',
+	age: null,
+	say: function () {
+		console.log('Hello');
+	}
+}
+
+userData.sayy?.()
+
+console.log(userData.skills?.js);
