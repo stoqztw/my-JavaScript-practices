ce2a378d2dcd636ebfe82597e16d5f30d70a2a0b
Artem <ap3994741@gmail.com>
2025-08-30 19:48:26 +0300

    lesson 20

---
+++ lesson_20/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// const bigInt = 8137589412895616457896378946578n;
+
+// const sameBigInt = BigInt(8137589412895616457896378946578);
+
+// console.log(2n > 1);
+
+let bigint = 2n;
+let num = 2;
+
+console.log(Number(bigint + BigInt(num)))
