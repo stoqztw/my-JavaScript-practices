fed659b336fb848e2159cec1136cc898007924e5
Artem <ap3994741@gmail.com>
2025-09-06 21:01:32 +0300

    fix timer

--- Food/src/js/script.js
+++ Food/src/js/script.js
@@ -95,41 +95,65 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	//HOMEWORK (TIMER)
 
-	const deadline = '2025-09-12';
+	const deadline = '2025-09-4';
 
 	function getTimeRemaining() {
-		const total = Date.parse(deadline) - Date.parse(Date()),
-			days = Math.floor(total / (1000 * 60 * 60 * 24)),
-			hours = Math.floor((total / (1000 * 60 * 60)) % 24),
-			minutes = Math.floor((total / (1000 * 60)) % 60),
-			seconds = Math.floor((total / 1000) % 60);
+		let days, hours, minutes, seconds;
+		const total = Date.parse(deadline) - Date.parse(Date());
 
+		if (total <= 0) {
 			return {
-				'total': total,
-				'days': days,
-				'hours': hours,
-				'minutes': minutes,
-				'seconds': seconds
+				'total': 0,
+				'days': 0,
+				'hours': 0,
+				'minutes': 0,
+				'seconds': 0
 			}
+		} else {
+			days = Math.floor(total / (1000 * 60 * 60 * 24));
+			hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+			minutes = Math.floor((total / (1000 * 60)) % 60);
+			seconds = Math.floor((total / 1000) % 60);
+		}
+
+		return {
+			'total': total,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		}
+	}
+
+	function getZero(num) {
+		if (num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
 	}
 
 	function setClock(selector, endtime) {
 		const timer = document.querySelector(selector),
-		days = timer.querySelector('#days'),
-		hours = timer.querySelector('#hours'),
-		minutes = timer.querySelector('#minutes'),
-		seconds = timer.querySelector('#seconds'),
-		timerUpdater = setInterval(updateClock, 1000);
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timerUpdater = setInterval(updateClock, 1000);
 
 		updateClock();
 
 		function updateClock() {
 			const time = getTimeRemaining(endtime);
 
-			days.innerHTML = time.days;
-			hours.innerHTML = time.hours;
-			minutes.innerHTML = time.minutes;
-			seconds.innerHTML = time.seconds;
+			days.innerHTML = getZero(time.days);
+			hours.innerHTML = getZero(time.hours);
+			minutes.innerHTML = getZero(time.minutes);
+			seconds.innerHTML = getZero(time.seconds);
+
+			if (time.total <= 0) {
+				clearInterval(timerUpdater);
+			}
 		}
 	}
 
