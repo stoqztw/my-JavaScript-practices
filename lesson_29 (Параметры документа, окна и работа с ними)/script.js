b79d9555319524ffc01ca10b0f149e5361791627
Artem <ap3994741@gmail.com>
2025-09-08 20:52:48 +0300

    lesson 29

--- lesson_29 (Параметры документа, окна и работа с ними)/script.js
+++ lesson_29 (Параметры документа, окна и работа с ними)/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const box = document.querySelector('.box');
+
+const width = box.offsetWidth,
+	height = box.scrollHeight;
+
+const btn = document.querySelector('button');
+
+// btn.addEventListener('click', (e) => {
+// 	// box.style.height = box.scrollHeight + 'px';
+// 	// console.log(box.scrollTop);
+// 	btn.style.width = btn.offsetWidth + 100 + 'px';
+// });
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+
+console.log(document.documentElement.scrollTop);
+
+const arrowButton = document.querySelector('.arrow');
+
+arrowButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	document.documentElement.scroll({
+		top: 0,
+		behavior: 'smooth'
+	});
+})
