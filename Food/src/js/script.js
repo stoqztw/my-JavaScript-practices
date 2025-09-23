64e54fb9d2aa1b396ad50335ac69b9f943c58249
Artem <ap3994741@gmail.com>
2025-09-23 14:55:49 +0300

    made server request for food web page

--- Food/src/js/script.js
+++ Food/src/js/script.js
@@ -230,7 +230,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		render() {
 			const element = document.createElement('div');
 
-			if (this.classes.length === 0) { 
+			if (this.classes.length === 0) {
 				this.classes = 'menu__item';
 				element.classList.add(this.classes);
 			} else {
@@ -279,4 +279,54 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.menu .container',
 		'menu__item'
 	).render();
+
+	//Forms
+
+	const forms = document.querySelectorAll('form');
+	const message = {
+		loading: 'Загрузка...',
+		succes: 'Спасибо! Скоро мы с вами свяжемся.',
+		failure: 'Что-то пошло не так...'
+	}
+
+	forms.forEach(item => { postData(item) });
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+			request.setRequestHeader('Contenr-type', 'application/json');
+
+			const formData = new FormData(form);
+			const object = {};
+
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.succes;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.failure;
+				}
+			});
+		});
+	}
 });
