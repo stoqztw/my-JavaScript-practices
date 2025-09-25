5aed666462d2475b50b0e5a7548872d5ed13d077
Artem <ap3994741@gmail.com>
2025-09-25 20:31:16 +0300

    homework

---
+++ lesson_38 (Метод перебора массива)/homeWork.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const films = [
+	{
+		name: 'Titanic',
+		rating: 9
+	},
+	{
+		name: 'Die hard 5',
+		rating: 5
+	},
+	{
+		name: 'Matrix',
+		rating: 8
+	},
+	{
+		name: 'Some bad film',
+		rating: 4
+	}
+];
+
+function showGoodFilms(arr) {
+
+}
+
+function showListOfFilms(arr) {
+
+}
+
+function setFilmsIds(arr) {
+
+}
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+
+}
