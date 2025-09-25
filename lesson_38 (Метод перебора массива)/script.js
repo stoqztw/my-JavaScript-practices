310ed92eda76c4030e2cf6937c4c3f31a98865b4
Artem <ap3994741@gmail.com>
2025-09-25 19:51:41 +0300

    new lesson, filter/map/some/every/reduce

---
+++ lesson_38 (Метод перебора массива)/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// filter 
+
+// const names = ['Alex', 'Danya', 'Voldemart', 'Ivan', 'Serji', 'Andrey'];
+
+// const shortNames = names.filter(function (name) {
+// 	return name.split('')[0] == 'A';
+// });
+
+// console.log(shortNames);
+
+// MAP
+
+// let answers = ['IVaN', 'HELlo', 'AHmuT'];
+
+// answers = answers.map(item => item.toLowerCase());
+
+// console.log(answers);
+
+// EVERY/SOME
+
+// const some = [4, 'asda', 'asdaafd'];
+
+// console.log(some.some(item => typeof(item) == 'number'));
+
+// const every = [4, 5, 5, 4];
+// console.log(some.every(item => typeof(item) == 'number'));
+// console.log(every.every(item => typeof(item) == 'number'));
+
+
+// REDUCE
+
+// const nums = ['apple', 'oranje', 'alabuga'];
+// const result = nums.reduce((sum, current) => `${sum}, ${current}`, 3);
+
+// console.log(result);
