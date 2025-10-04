c12d9c94a739e88b7cf2ec3aa9536a323c26fb08
atsq <ap3994741@gmail.com>
2025-10-04 23:58:10 +0300

    localstorage, save obj

--- lesson_39 (LocalStorage)/script.js
+++ lesson_39 (LocalStorage)/script.js
@@ -11,6 +11,31 @@ if (localStorage.getItem('isChecked')) {
 	checkBox.checked = true;
 }
 
+if (localStorage.getItem('bg') === 'changed') {
+	form.style.backgroundColor = 'red';
+}
+
 checkBox.addEventListener('change', () => {
 	localStorage.setItem('isChecked', true);
 });
+
+changeColorButton.addEventListener('click', () => {
+	if (localStorage.getItem('bg') === 'changed') {
+		localStorage.removeItem('bg');
+		form.style.backgroundColor = 'white';
+	} else {
+		localStorage.setItem('bg', 'changed');
+		form.style.backgroundColor = 'red';
+	}
+});
+
+
+const persone = {
+	name: 'Alex',
+	age: 28
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')))
