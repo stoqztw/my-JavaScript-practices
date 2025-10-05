7d3e9a3456f3b852d925c0e3183a76ff16a37e52
atsq <ap3994741@gmail.com>
2025-10-05 14:55:33 +0300

    new lesson, regular expression

---
+++ lesson_40 (регулярные выражения)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /replcae/f
+
+// const ans = prompt('Введите цифры');
+
+// const reg = /\d/g;
+// console.log(+ans.match(/\d/g).join(''))
+
+// console.log(ans.search(/n/i))
+// console.log(ans.match(/n/ig)[0]);
+
+// const pass = prompt('password');
+
+// console.log(pass.replace(/\./g, ''))
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+const str = 'My name is R2D2';
+
+console.log(str.match(/\D/ig))
