f2d63ec72ba68da50984dbf8f2a581916873a0f7
atsq <ap3994741@gmail.com>
2025-10-11 18:22:17 +0300

    new lesson, try catch

---
+++ lesson_46 (try, catch)/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+try {
+	console.log(a);
+} catch(e) {
+	console.log('error');
+} finally {
+	console.log('final');
+}
+
+console.log('Still normal');
+
+//такая конструкция использутся для запуска криптов для определенных страниц
